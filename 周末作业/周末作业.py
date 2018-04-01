aad537d29e0255b5608e4544e0958e3ceb0f9ea9
1803 <767016057@qq.com>
2018-04-01 14:29:02 +0800

    test

---
+++ 周末作业/周末作业.py
@@ -0,0 +1,40 @@
+print("一年=%s秒"%(60*60*24*365))
+print("一公里=%s毫米"%(1000*1000))
+print("*"*50)
+
+
+
+x = 2
+print(x**2)
+print(6*(x/7))
+print(6/(4+5*x))
+print("*"*50)
+
+
+#print('*'*50)
+name =input "陈怡杰"
+gender =input "女"
+phone =input "17610463986"
+addir =input "河南南阳"
+email =input "767016057@qq.com"
+print('*'*50)
+print("姓名%s\n"%name,"性别%s\n"%gender,"电话：%s\n"%phone)
+print('*'*50)
+account =input "chenyijie"
+password =input "520134"
+nick-name =input "陈怡杰"
+money = 200000
+need-money =input 200000
+sum = money - int(200000）
+print("%s\n密码520134\n姓名：%s\n原有存款为%d\n取款金额为%s\n剩余%d"%(account,nick-name,money,need-money,sum))
+
+name = input"陈怡杰"
+age = int(input(17))
+sexxy = input("女")
+high = float(input(110))
+case = input("学生")
+money = int(input(10000))
+print('*'*10)
+print("职位%s\nmoney%d\n身高%f\n"%(case,money,high))
+print('*'*10)
+
